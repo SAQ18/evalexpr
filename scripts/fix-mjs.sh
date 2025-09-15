958a7c80a47a3cf74c4860af595cf84f00181a15
Muhammad Saqib <msaqib1996@gmail.com>
2025-09-15 19:37:53 +0100

    fix: updated fix-mjs.sh script to also rename files in subfolders of dist/esm/**/*.js

--- scripts/fix-mjs.sh
+++ scripts/fix-mjs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# This script renames .js files in the ./dist/esm directory to .mjs
+# This script renames .js files in the ./dist/esm directory and its subfolders to .mjs
 
-for file in ./dist/esm/*.js; do
+# Find all .js files in ./dist/esm and its subdirectories
+find ./dist/esm -name "*.js" -type f | while read -r file; do
   echo "Updating $file contents..."
   sed -i '' "s/\.js'/\.mjs'/g" "$file"
   echo "Renaming $file to ${file%.js}.mjs..."
